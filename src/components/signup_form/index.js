9d3e789e882aee4e1663349b089c66763469f9dc
DylanC <88355961+comeaudc@users.noreply.github.com>
2024-03-22 15:56:18 -0400

    Initial Commit

--- src/components/signup_form/index.js
+++ src/components/signup_form/index.js
@@ -1,20 +1,51 @@
-import React from 'react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom/dist';
+import { useAuth } from '../../contexts/auth/auth_context';
 
 const SignUp = ({ setNewUser }) => {
+  const nav = useNavigate();
+  const { signUp } = useAuth();
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: '',
+  });
+
   const handleClick = () => {
     setNewUser(false);
+  };
+
+  const onChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const onSubmit = async(e) => {
+    e.preventDefault();
+    if (formData.password !== formData.password2) {
+      console.log('Passwords Dont Match');
+    } else {
+      await signUp(formData)
+      nav('/dashboard')
+    }
   };
 
   return (
     <div className='forms'>
       <h2>SignUp</h2>
-      <form autoComplete='off'>
+      <form onSubmit={(e) => onSubmit(e)} autoComplete='off'>
         <label htmlFor='name1'>Name: </label>
         <input
           type='text'
           id='name1'
           name='name'
           placeholder='First and Last Name'
+          onChange={(e) => {
+            onChange(e);
+          }}
         />
         <label htmlFor='email1'>Email: </label>
         <input
@@ -22,6 +53,9 @@ const SignUp = ({ setNewUser }) => {
           id='email1'
           name='email'
           placeholder='Email'
+          onChange={(e) => {
+            onChange(e);
+          }}
         />
         <label htmlFor='password1'>Password: </label>
         <input
@@ -30,6 +64,9 @@ const SignUp = ({ setNewUser }) => {
           name='password'
           placeholder='Password'
           minLength='6'
+          onChange={(e) => {
+            onChange(e);
+          }}
         />
         <input
           type='password'
@@ -37,6 +74,9 @@ const SignUp = ({ setNewUser }) => {
           name='password2'
           placeholder='Confirm Password'
           minLength='6'
+          onChange={(e) => {
+            onChange(e);
+          }}
         />
         <button type='submit'>Sign Up</button>
       </form>
--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import { useAuth } from '../../contexts/auth/auth_context';
 
 const Dashboard = () => {
+  const { logout } = useAuth();
+
+  const handleClick = () => {
+    logout();
+  };
+
   return (
     <div>
       <h1>You are logged in so can see this</h1>
-      <button>LogOut</button>
+      <button onClick={handleClick}>LogOut</button>
     </div>
   );
 };
